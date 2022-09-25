78f45eaff5388d9aa56d68efde25ead73e1e94d6
Sreelakshmi-1997 <114090038+Sreelakshmi-1997@users.noreply.github.com>
2022-09-25 19:28:00 +0530

    Create WebDriverUtilities.java

---
+++ WebDriverUtilities.java
@@ -0,0 +1,30 @@
+package testNGpackage;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class WebDriverUtilities {
+public static WebDriver driver;
+	
+	public static void launchBrowser(String driverpath,String url) {
+		
+		
+		System.setProperty("webdriver.chrome.driver",driverpath);
+		driver =new ChromeDriver();
+		driver.get(url);
+		driver.manage().window().maximize();
+		
+	}
+	public void click(By value)
+	{
+		driver.findElement(value).click();
+	}
+	public void sendkeys(By value,String value1)
+	{
+		driver.findElement(value).sendKeys(value1);;
+	}
+	
+}
+
+
